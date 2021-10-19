2d023b17e0bcb0d436aa5ee8807dbbec2b907bb1
Taniguchi1204 <tatsuya19951204@gmail.com>
2021-10-19 18:55:37 +0900

    practice for test

--- array.rb
+++ array.rb
@@ -1,54 +1,35 @@
-p [].class #=> Array.class
-a = ["a", "b", "c"]
-a[5] = "f"
-p a #=> ["a", "b", "c", nil, nil, "f"]
-a.delete_at(2)
-p a #=> ["a", "b", nil, nil, "f"]
 
-# 配列から多重代入することができる（divmodは商と余りを配列にして返す）
-qoutient, remainder = 14.divmod(3)
-puts "商：#{qoutient}, 余り：#{remainder}" #=> 商：4, 余り：2
-
-array = (1..6).to_a
-array.delete_if do |n|
-  n.odd? #奇数の場合は削除
+def number(limit)
+  count = 0
+  (10..limit).each do |n|
+    count_within_3 = 0
+    loop do
+      n = n.to_s.scan(/\w/).inject { |result, n| result.to_i * n.to_i}
+      count_within_3 += 1
+      break if n < 10
+    end
+    if count_within_3 == 3
+      count += 1
+    end
+  end
+  p count
 end
 
-p array #=>[2,6]
+number(1000000)
 
-number = [1,2,3,4,5]
-new_number = number.map { |n| n * 10 }
-p new_number
+# require 'minitest/autorun'
 
-three_array = (1..100).select {|n| n % 3 == 0 || n.to_s.include?('3')}
-p three_array.count
-
-p (1..5).class #=> Rangeクラス
-
-p [*1..6] #=> [1, 2, 3, 4, 5, 6]
-
-def to_int(hex)
-  hex.scan(/\w\w/).map(&:hex)
-end
-
-p to_int('#12abcd')
-
-a = [1,2,3,5,6,7,8,7]
-b = [*1..7]
-
-p b - a
-
-a = ("1,2,3,4abc").scan(/\w/)
-p a
-
-a = Array.new(5){"dafault"}
-a[1].upcase!
-p a
-
-a = "abcde"
-p a[2]
-p a[1, 3]
-
-a = [[20,30],[40,50],[60,70]]
-area = a.map {|l, w| l * w}
-p area
+# class NumberTest < Minitest::Test
+#   def test_number
+#     assert_equal 0, number(10)
+#     assert_equal 0, number(20)
+#     assert_equal 0, number(30)
+#     assert_equal 1, number(40)
+#     assert_equal 3, number(50)
+#     assert_equal 6, number(60)
+#     assert_equal 9, number(70)
+#     assert_equal 13, number(80)
+#     assert_equal 17, number(90)
+#     assert_equal 23, number(100)
+#   end
+# end
---
+++ sample.rb
@@ -0,0 +1,3 @@
+input = gets.chomp
+a = 'number.each {|n| n**4}'
+p a == input ? 'true' : 'false'
