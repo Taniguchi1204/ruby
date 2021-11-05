77d8aeb716b95afb1d706714264514f4c742fa10
Taniguchi1204 <tatsuya19951204@gmail.com>
2021-11-05 22:59:38 +0900

    7章０台

--- lib/class.rb
+++ lib/class.rb
@@ -16,6 +16,20 @@ class User
   end
 end
 
+class Product
+  DEFAULT_PRICE = 0
+
+  attr_reader :name, :price
+
+  def initialize(name, price = DEFAULT_PRICE)
+    @name = name
+    @price = price
+  end
+end
+
+product = Product.new("A free movie")
+puts product.price
+
 names = ["Alice", "Bob", "Carol"]
 
 users = User.create_users(names)
